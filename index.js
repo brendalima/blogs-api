8e8bfdd5e2ce8f7f8e61b26a79656407c1e4a667
brendalima <brendalima@>
2021-05-30 21:11:45 -0300

    creates database

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const routes = require('./routes');
+require('dotenv').config();
 
 const app = express();
+app.use(routes.userRoutes);
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
